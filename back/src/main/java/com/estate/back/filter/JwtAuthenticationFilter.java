6ea3c3fe8dedf317f349ca37c76e41c6e358469b
parksungeon <un109101@naver.com>
2024-04-24 12:50:36 +0900

    2024.04.24 12:50

--- back/src/main/java/com/estate/back/filter/JwtAuthenticationFilter.java
+++ back/src/main/java/com/estate/back/filter/JwtAuthenticationFilter.java
@@ -1,20 +1,39 @@
 package com.estate.back.filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.estate.back.entity.UserEntity;
+import com.estate.back.provider.JwtProvider;
+import com.estate.back.repository.UserRepository;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 // Spring Security Filter Chain에 추가할 JWT 필터
 // - Request 객체로부터 Header 정보를 받아와서 Header에 있는 Authorization 필드의 Bearer 토큰 값을 가져와서 JWT 검증
 // - 접근 주체의 권한을 확인하여 권한 등록
-
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
 
     // JwtAuthenticationFilter의 실제 동작
     @Override
@@ -29,6 +48,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 filterChain.doFilter(request, response);
                 return;
             }
+            // JWT 검증
+            String userId = jwtProvider.validate(token);
+            if (userId == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            // 접근 주체에 권한을 확인
+            UserEntity userEntity = userRepository.findByUserId(userId);  //  예상되는 기대 갯수 0~1개 (0 = null)
+            if (userEntity == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String role = userEntity.getUserRole();     // 인증 절차
+
+            // 권한 테이블(리스트) 생성
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(role));   // SimpleGrantedAuthority : 단일권한 객체 문자열 전달 -> 문자열의 규칙 존재 role_
+
+            // Context에 접근 주체 정보를 추가
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, null, authorities);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(securityContext);
 
         } catch (Exception exception) {
             exception.printStackTrace();
