e4bfb68abf6deb562a5465baeae932dca11a4c02
parksungeon <un109101@naver.com>
2024-04-26 09:51:53 +0900

    2024.04.26 9:51

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,3 +6,12 @@ function App() {
 }
 
 export default App;
+
+// - authentication (로그인, 회원가입)
+// - service
+//     - local (지역평균)
+//     - ratio (비율 계산)
+//     - qna (Q&A)
+//         - :boardNumber (Q&A 상세보기)
+//         - write (Q&A 작성)
+//         - update/:boardNumber (Q&A 수정)
