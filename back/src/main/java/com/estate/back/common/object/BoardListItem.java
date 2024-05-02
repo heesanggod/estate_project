9b25d8f76b21fd4e61427b0b43efcfce8b3772bd
parksungeon <un109101@naver.com>
2024-05-02 17:47:55 +0900

    2024.05.02 5:47

--- back/src/main/java/com/estate/back/common/object/BoardListItem.java
+++ back/src/main/java/com/estate/back/common/object/BoardListItem.java
@@ -19,16 +19,21 @@ public class BoardListItem {
     private Integer viewCount;
 
     private BoardListItem(BoardEntity boardEntity) throws Exception {
+        // 날짜 포맷 변환을 위한 SimpleDateFormat 객체 생성
         SimpleDateFormat simpleDateFormat = new  SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // BoardEntity 객체에서 작성 일자를 받아 Date 객체로 변환
         Date datetime = simpleDateFormat.parse(boardEntity.getWriteDatetime());
+        // Date type을 SimpleDateFormat의 패턴 yy.MM.dd 변환
         simpleDateFormat = new SimpleDateFormat("yy.MM.dd");
         String writeDatetime = simpleDateFormat.format(datetime);
 
+        // 작성자 아이디 가림
         String writerId = boardEntity.getWriterId();
         writerId =
         writerId.substring(0, 1) + 
         "*".repeat(writerId.length() - 1);
 
+        // BoardListItem 객체의 필드에 값을 할당
         this.receptionNumber = boardEntity.getReceptionNumber();
         this.status = boardEntity.getStatus();
         this.title = boardEntity.getTitle();
